53ba11da1ea0dcdd78ee2aab30983f251640b370
Jonathan West <jonathan.west@bie.edu>
2016-02-23 18:14:35 -0700

    removed service from mars_pi node

--- src/mars_pi/src/mars_pi_node.cpp
+++ src/mars_pi/src/mars_pi_node.cpp
@@ -1,19 +1,11 @@
 #include "ros/ros.h"
-#include "mars_pi/status.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 
 int count = 0;
-bool cbService(mars_pi::status::Request &req,
-		mars_pi::status::Response &res)
-{
-	res.status = count++;
-	return true;
-}
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "mars_pi_node");
 	ros::NodeHandle n;
-	ros::ServiceServer service = n.advertiseService("mars_pi_status", cbService);
 	ros::Publisher status_pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("status",1000);
 	ros::Rate loop_rate(1);
 	while(ros::ok()) {
